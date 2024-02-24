8c29ca2520671ad136d4e8752383f1335d0d79ef
KentHefley <hefleykent@gmail.com>
2024-02-24 15:08:35 -0600

    finished project.

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,41 @@
+import { fullBlog } from "@/app/lib/interface"
+import { client, urlFor } from "@/app/lib/sanity"
+import Image from "next/image"
+import {PortableText} from '@portabletext/react'
+
+async function getData(slug: string) {
+    const query = `
+    *[_type == 'blog' && slug.current == '${slug}'] {
+        "currentSlug" : slug.current,
+          title,
+          content,
+          titleImage
+      }[0]
+    `
+
+    const data = await client.fetch(query)
+    return data
+}
+
+export default async function BlogArticle({params}: {params: {slug: string}}) {
+    const data: fullBlog = await getData(params.slug)
+    console.log(data)
+  return (
+    <div className="mt-8">
+        <h1>
+            <span className="block text-base text-center text-primary font-semibold tracking-wide uppercase">Kent Hefley - blog</span>
+            <span className='mt-2 block text-3xl text-center leading-8 font-bold tracking-tight sm:text-4xl'>{data.title}</span>
+        </h1>
+        <Image 
+          src={urlFor(data.titleImage).url()} 
+          width={800} height={800} 
+          alt={`${data.title} image`}
+          priority
+          className="rounded-lg mt-8 border"
+          />
+          <div className="mt-16 prose lg:prose-lg prose:li:marker:text-primary dark:prose-invert">
+            <PortableText value={data.content}/>
+          </div>
+    </div>
+  )
+}
